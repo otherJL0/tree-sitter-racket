2cb0881b7adeec5c219df041f2d5e2b6ab53ab20
otherJL0 <jonathanglopez@gmail.com>
2021-12-07 15:36:20 -0500

    Format grammar.js file

--- grammar.js
+++ grammar.js
@@ -1,48 +1,52 @@
 module.exports = grammar({
-  name: 'racket',
+  name: "racket",
 
-  extras: $ => [ $._comment, /\s+/ ],
+  extras: ($) => [$._comment, /\s+/],
 
   rules: {
-    source_file: $ => seq(
-      $.lang_line,
-      repeat($.datum)
-    ),
+    source_file: ($) =>
+      seq(
+        $.lang_line,
+        repeat($.datum),
+      ),
 
-    lang_line: $ => seq('#lang', $.symbol),
+    lang_line: ($) => seq("#lang", $.symbol),
 
-    datum: $ => choice(
-      $.number,
-      $.symbol,
-      $.string,
-      $.boolean,
-      $.list_or_pair,
-      $.quoted_datum
-    ),
+    datum: ($) =>
+      choice(
+        $.number,
+        $.symbol,
+        $.string,
+        $.boolean,
+        $.list_or_pair,
+        $.quoted_datum,
+      ),
 
-    boolean:   $ => choice($.true_lit, $.false_lit),
-    true_lit:  $ => token(choice("#t", "#T")),
-    false_lit: $ => token(choice("#f", "#F")),
+    boolean: ($) => choice($.true_lit, $.false_lit),
+    true_lit: (_$) => token(choice("#t", "#T")),
+    false_lit: (_$) => token(choice("#f", "#F")),
 
-    string: $ => seq('"', /[^"]*/, '"'),    // TODO: escaped quotes
+    string: (_$) => seq('"', /[^"]*/, '"'), // TODO: escaped quotes
 
-    quoted_datum: $ => seq(choice("'", "`"), $.datum),
+    quoted_datum: ($) => seq(choice("'", "`"), $.datum),
 
-    number: $ => token(choice(
-      /\d+([\./]\d+)?/,
-      /#x[0-9A-Fa-f]+/
-    )),
+    number: (_$) =>
+      token(choice(
+        /\d+([\./]\d+)?/,
+        /#x[0-9A-Fa-f]+/,
+      )),
 
-    symbol: $ => /[^()\[\]{}",'`;#|\s\\]+/,   // TODO: verbatim symbols with |
+    symbol: (_$) => /[^()\[\]{}",'`;#|\s\\]+/, // TODO: verbatim symbols with |
 
-    list_or_pair: $ => choice(
-      seq("(", repeat(choice($.datum, $.dot)), ")"),
-      seq("[", repeat(choice($.datum, $.dot)), "]"),
-      seq("{", repeat(choice($.datum, $.dot)), "}"),
-    ),
+    list_or_pair: ($) =>
+      choice(
+        seq("(", repeat(choice($.datum, $.dot)), ")"),
+        seq("[", repeat(choice($.datum, $.dot)), "]"),
+        seq("{", repeat(choice($.datum, $.dot)), "}"),
+      ),
 
-    dot: $ => ".",
+    dot: (_$) => ".",
 
-    _comment: $ => token(seq(';', /.*/))
-  }
+    _comment: (_$) => token(seq(";", /.*/)),
+  },
 });
